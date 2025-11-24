adfa07965c92f40e37bc74ea538546ecbc648dd8
Theyvison <theyvison.jf@hotmail.com>
2025-11-24 14:41:07 -0300

    configurações globais para os clientes

--- src/main/java/io/github/theyvison/libraryapi/security/CustomRegisteredClientRepository.java
+++ src/main/java/io/github/theyvison/libraryapi/security/CustomRegisteredClientRepository.java
@@ -6,13 +6,17 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class CustomRegisteredClientRepository implements RegisteredClientRepository {
 
-    private ClientService clientService;
+    private final ClientService clientService;
+    private final TokenSettings tokenSettings;
+    private final ClientSettings clientSettings;
 
     @Override
     public void save(RegisteredClient registeredClient) {
@@ -41,6 +45,8 @@ public class CustomRegisteredClientRepository implements RegisteredClientReposit
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings)
                 .build();
     }
 }
