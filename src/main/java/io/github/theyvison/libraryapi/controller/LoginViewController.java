3548aa95437d1ad69661e3e70a1750531ad6d337
Theyvison <theyvison.jf@hotmail.com>
2025-11-25 11:10:05 -0300

    se autenticando com authorization grant type

--- src/main/java/io/github/theyvison/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/theyvison/libraryapi/controller/LoginViewController.java
@@ -3,6 +3,7 @@ package io.github.theyvison.libraryapi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,5 +18,11 @@ public class LoginViewController {
     @ResponseBody
     public String paginaHome(Authentication authentication) {
         return "Olá " + authentication.getName();
+    }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAuthorizationCode(@RequestParam("code") String code) {
+        return "Seu authorization code: " + code;
     }
 }
